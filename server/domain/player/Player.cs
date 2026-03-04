ebc2cf74ed3388c16816b20253d1fb53943c4b20
Ayumu Ochiai <ochiohita@gmail.com>
2026-03-04 16:29:17 +0900

    feat: Playerへレベル関連の関数を実装.

--- server/domain/player/Player.cs
+++ server/domain/player/Player.cs
@@ -1,11 +1,14 @@
 namespace server.domain.player;
 
-public class Player(PlayerId id, string name)
+public class Player(PlayerId id, string name, int level, int exp, BaseStatus status)
 {
     public const int NameMaxLength = 20;
 
     public PlayerId Id { get; } = id;
     public string Name { get; private set; } = ValidateName(name);
+    public int Level { get; private set; } = level;
+    public int Exp { get; private set; } = exp;
+    public BaseStatus Status {get; set;} = status;
 
     public void UpdateName(string name)
     {
@@ -21,5 +24,19 @@ public class Player(PlayerId id, string name)
         }
 
         return normalized;
+    }
+    public void GainExp(int exp)
+    {
+        Exp += exp;
+    }
+    public bool LevelUp()
+    {
+        if(Exp >= Level * 10)
+        {
+            Exp -= Level * 10;
+            Level++;
+            return true;
+        }
+        return false;
     }
 }
