3a2276901fe53643921bd608f8d3a1e645c0ef28
Ayumu Ochiai <ochiohita@gmail.com>
2026-03-08 15:28:03 +0900

    feat: ドメイン層へジョブを追加.

---
+++ server/domain/player/Job.cs
@@ -0,0 +1,11 @@
+namespace server.domain.player;
+
+public enum Job
+{
+    Apprentice = 1,
+    Warrior = 2,
+    Guardian = 3,
+    Mage = 4,
+    Priest = 5,
+    Ranger = 6
+}
--- server/domain/player/Player.cs
+++ server/domain/player/Player.cs
@@ -2,10 +2,11 @@ using server.shared.constants.player;
 
 namespace server.domain.player;
 
-public class Player(PlayerId id, string name, int level, int exp, Status status)
+public class Player(PlayerId id, string name, int level, int exp, Status status, Job job = Job.Apprentice)
 {
     public PlayerId Id { get; } = id;
     public string Name { get; private set; } = ValidateName(name);
+    public Job Job { get; private set; } = job;
     public int Level { get; private set; } = ValidateLevel(level);
     public int Exp { get; private set; } = exp;
     public Status Status { get; private set; } = status ?? throw new ArgumentNullException(nameof(status));
@@ -13,6 +14,11 @@ public class Player(PlayerId id, string name, int level, int exp, Status status)
     public void UpdateName(string name)
     {
         Name = ValidateName(name);
+    }
+
+    public void UpdateJob(Job job)
+    {
+        Job = job;
     }
 
     public void UpdateStatus(Status status)
