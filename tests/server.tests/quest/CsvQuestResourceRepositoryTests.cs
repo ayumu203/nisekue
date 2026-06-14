2dc2b2db187b5f418f878d8cd575a5e42685cbd2
Ayumu Ochiai <ochiai.ayumu.23@shizuoka.ac.jp>
2026-06-14 14:10:35 +0900

    feat: ボスラッシュを実装.

--- tests/server.tests/quest/CsvQuestResourceRepositoryTests.cs
+++ tests/server.tests/quest/CsvQuestResourceRepositoryTests.cs
@@ -40,6 +40,24 @@ public class CsvQuestResourceRepositoryTests
     }
 
     [Fact]
+    public async Task GetByStageCodeAsync_WhenBossRushExists_ReturnsBossRushDefinition()
+    {
+        var repository = new CsvQuestStageRepository();
+
+        var stage = await repository.GetByStageCodeAsync("boss-rush");
+
+        stage.Should().NotBeNull();
+        stage!.Id.Value.Should().Be(12);
+        stage.Name.Should().Be("ボスラッシュ");
+        stage.RecommendedLevel.Should().Be(2000);
+        stage.MinimumEntryLevel.Should().Be(1000);
+        stage.Floors.Should().HaveCount(6);
+        stage.Floors.Should().OnlyContain(x => x.FloorType == server.domain.quest.enums.FloorType.Boss);
+        stage.Floors.Should().OnlyContain(x => x.Placements.Count == 1);
+        stage.Floors.Select(x => x.Placements[0].EnemyDefinitionId.Value).Should().ContainInOrder(82, 83, 84, 85, 86, 87);
+    }
+
+    [Fact]
     public async Task GetAsync_WhenTrialForestEnemiesExist_ReturnsExpectedImagePaths()
     {
         var repository = new CsvQuestEnemyDefinitionRepository();
@@ -72,6 +90,24 @@ public class CsvQuestResourceRepositoryTests
     }
 
     [Fact]
+    public async Task GetAsync_WhenBossRushBossesExist_ReturnsExpectedDefinitions()
+    {
+        var repository = new CsvQuestEnemyDefinitionRepository();
+
+        var firstBoss = await repository.GetAsync(new QuestEnemyDefinitionId(82));
+        var lastBoss = await repository.GetAsync(new QuestEnemyDefinitionId(87));
+
+        firstBoss.Should().NotBeNull();
+        firstBoss!.Name.Should().Be("トオリャンセ・極");
+        firstBoss.ImagePath.Should().Be("image/battle/Enemy43.png");
+        firstBoss.MoveIds.Select(x => x.Id).Should().ContainInOrder(613, 126, 414);
+        lastBoss.Should().NotBeNull();
+        lastBoss!.Name.Should().Be("墓守の王・極");
+        lastBoss.ImagePath.Should().Be("image/battle/Enemy117.png");
+        lastBoss.MoveIds.Select(x => x.Id).Should().ContainInOrder(618, 611, 612);
+    }
+
+    [Fact]
     public async Task GetForStartAsync_WhenTrialForestRequiresNpcFill_ReturnsHighLevelTemplates()
     {
         var repository = new CsvQuestNpcTemplateRepository();
@@ -89,6 +125,18 @@ public class CsvQuestResourceRepositoryTests
         var repository = new CsvQuestNpcTemplateRepository();
 
         var templates = await repository.GetForStartAsync(new QuestStageId(8), 4);
+
+        templates.Should().HaveCount(4);
+        templates.Should().OnlyContain(x => x.Level == 2000);
+        templates.Select(x => x.Id.Value).Should().OnlyContain(id => id >= 30 && id <= 34);
+    }
+
+    [Fact]
+    public async Task GetForStartAsync_WhenBossRushRequiresNpcFill_ReturnsEndgameTemplates()
+    {
+        var repository = new CsvQuestNpcTemplateRepository();
+
+        var templates = await repository.GetForStartAsync(new QuestStageId(12), 4);
 
         templates.Should().HaveCount(4);
         templates.Should().OnlyContain(x => x.Level == 2000);
